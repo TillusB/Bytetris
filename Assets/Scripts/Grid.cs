7964990422ede4cdbba10a66fb4aece589a361fa
TillusB <tbalbach@gmx.net>
2018-01-16 18:31:14 +0100

    made some block prefabs, started on a basic grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public int width = 10;
+    public int height = 20;
+    public Transform[,] grid;
+
+	// Use this for initialization
+	void Start () {
+        grid = new Transform[width, height];
+        ClearGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClearGrid()
+    {
+        Transform block;
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                block = grid[x, y];
+                if (block != null)
+                {
+                    Destroy(block);
+                    block = null;
+                }
+            }
+        }
+    }
+}
