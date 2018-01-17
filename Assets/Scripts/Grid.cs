7b547b1a49aa75e3e7d89eb77ed4bdb415778929
TillusB <tbalbach@gmx.net>
2018-01-17 19:25:48 +0100

    automated block spawning and removing lines etc

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,8 +16,27 @@ public class Grid : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Debug.DrawLine(new Vector2(0, 0), new Vector2(width, 0));
+        Debug.DrawLine(new Vector2(0, 0), new Vector2(0, height));
+        
+
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y]) Gizmos.color = Color.red;
+                else Gizmos.color = Color.green;
+                Gizmos.DrawCube(new Vector3(x, y, 0), new Vector3(.5f, .5f, .5f));
+            }
+        }
+    }
+#endif
 
     public void ClearGrid()
     {
@@ -34,4 +54,56 @@ public class Grid : MonoBehaviour {
             }
         }
     }
+
+    internal void CheckLineFull(int yCoordinate)
+    {
+        bool isfull = true;
+        for(int x = 0; x < width; x++)
+        {
+            if (!grid[x, yCoordinate])
+            {
+                isfull = false;
+            }
+        }
+
+        if (isfull)
+        {
+            ClearLine(yCoordinate);
+        }
+    }
+
+    private void ClearLine(int yCoordinate)
+    {
+        Transform clearBlock;
+        for(int x = 0; x < width; x++)
+        {
+            clearBlock = grid[x, yCoordinate];
+            if (clearBlock)
+            {
+                grid[x, yCoordinate] = null;
+                Destroy(clearBlock.gameObject);
+            }
+        }
+
+        DropLinesAbove(yCoordinate);
+    }
+
+    private void DropLinesAbove(int yCoordinate) // Drop all lines above this height. Used after lines are cleared
+    {
+        Transform moveBlock;
+        for (int y = yCoordinate; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                moveBlock = grid[x, y];
+                if (moveBlock) // There is a block here
+                {
+                    grid[x, y] = null;
+                    grid[x, y - 1] = moveBlock;
+                    moveBlock.position = new Vector3(x, y - 1, 0);
+                }
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public Block[] availableBlocks;
+
+    private Grid gridInstance;
+    private Block nextBlock;
+    private Vector2 spawnPoint;
+    private InputManager input;
+
+    private Block currentBlock;
+    public Block CurrentBlock
+    {
+        get { return currentBlock; }
+        private set {
+            currentBlock = value;
+            input.CurrentBlock = currentBlock;
+        }
+    }
+
+	void Start () {
+        gridInstance = GetComponent<Grid>();
+        input = GetComponent<InputManager>();
+
+        spawnPoint = new Vector2(Mathf.RoundToInt(gridInstance.width / 2), gridInstance.height - 4);
+
+        nextBlock = availableBlocks[Random.Range(0, availableBlocks.Length - 1)];
+    }
+
+    void FixedUpdate () {
+		if(currentBlock == null || !currentBlock.active)
+        {
+            CurrentBlock = Instantiate(nextBlock, spawnPoint, Quaternion.identity);
+            nextBlock = availableBlocks[Random.Range(0, availableBlocks.Length - 1)];
+        }
+	}
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F1))
+        {
+            for(int i = 0; i < availableBlocks.Length; i++)
+            {
+                availableBlocks[i] = availableBlocks[0];
+            }
+            nextBlock = availableBlocks[0];
+            Destroy(currentBlock.gameObject);
+            CurrentBlock = Instantiate(nextBlock, spawnPoint, Quaternion.identity);
+        }
+    }
+}
