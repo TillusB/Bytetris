d9e8b1e69af6fc39151655a2d439582d7a2e9b8a
TillusB <tbalbach@gmx.net>
2018-01-18 21:48:31 +0100

    last chance and drop mechanic, docu

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,16 +9,27 @@ public class Block : MonoBehaviour {
     private float timeSinceMove = 0;
     private Grid gridInstance;
     private Vector2[] previousPos = new Vector2[4];
+    private bool lastChance = false;
     
-    public float moveDelay = 0.5f;
+    public static float moveDelay = 0.5f;
     public bool active = true;
 
 	void Start () {
-        gridInstance = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>(); //TODO: Maybe go through manager
+        gridInstance = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
 
-        SetPreviousPos();
+        SetPreviousPos(); // Register this blocks' start position on the grid
 	}
 	
+    /// <summary>
+    /// 
+    /// In Update we wait for the current delay - one "tick" of the game so to say - to move the current block downwards if possible.
+    /// If a piece can't move downwards anymore, lastChance gets flagged one tick before the piece gets locked in position for last adjustments by the player.
+    /// 
+    /// Movedelay is the value that defines the length of a tick. It gets adjusted in the Grid class, whenever a line gets cleared.
+    /// This way the pace picks up the better a player is doing.
+    /// 
+    /// 
+    /// </summary>
 	void Update () {
         if (active)
         {
@@ -26,9 +37,8 @@ public class Block : MonoBehaviour {
             if(timeSinceMove >= moveDelay)
             {
                 timeSinceMove = 0;
-                Move(Vector2.down);
 
-                if (!NextPosAvailable(Vector2.down))
+                if (!NextPosAvailable(Vector2.down) && lastChance)
                 {
                     active = false;
 
@@ -37,11 +47,23 @@ public class Block : MonoBehaviour {
                         gridInstance.CheckLineFull(Mathf.RoundToInt(child.position.y));
                     }
                 }
+                else if(!NextPosAvailable(Vector2.down) &! lastChance)
+                {
+                    lastChance = true;
+                    return;
+                }
+                else
+                {
+                    Move(Vector2.down);
+                }
             }
         }
 	}
 
-
+    /// <summary>
+    /// Move block in a direction if possible
+    /// </summary>
+    /// <param name="movementV">Direction</param>
     public void Move(Vector2 movementV)
     {
         if (NextPosAvailable(movementV))
@@ -50,21 +72,41 @@ public class Block : MonoBehaviour {
 
             UpdatePositionToGrid();
             SetPreviousPos();
+            //lastChance = false; 
+
         }
     }
 
+    /// <summary>
+    /// Rotate the block by 90 degrees if there is space.
+    /// Otherwise rotate it back to its previous orientation.
+    /// 
+    /// TODO: allow rotation if the block is only obstructed horizontally -> push the block away from the obstacle instead.
+    /// </summary>
     public void Rotate()
     {
-        
         transform.Rotate(new Vector3(0, 0, -90));
-        
+        if (!NextPosAvailable(Vector3.zero))
+        {
+            transform.Rotate(new Vector3(0, 0, 90));
+            return;
+        }
         UpdatePositionToGrid();
         SetPreviousPos();
     }
 
+    /// <summary>
+    /// Move the block all the way down in an instant.
+    /// </summary>
     internal void Drop()
     {
-        throw new NotImplementedException();
+        while (NextPosAvailable(Vector2.down)){
+            Move(Vector2.down);
+            UpdatePositionToGrid();
+            SetPreviousPos();
+        }
+        lastChance = true;
+        timeSinceMove = moveDelay;
     }
 
     private void UpdatePositionToGrid()
