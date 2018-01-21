b899958dbabe920071ae0cd4af3881dc8eafcedb
TillusB <tbalbach@gmx.net>
2018-01-21 16:50:38 +0100

    adjusted prefabs, inheritance for special blocks

---
+++ Assets/Scripts/Blocks/IBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IBlock : Block {
+
+	// Because the IBlock doesnt have a middle block to rotate around we have to arrange the children differently and compensate for their offset here.
+	protected override void Start () {
+        base.Start();
+        transform.Translate((Vector2.left+Vector2.up) / 2);
+    }
+}
---
+++ Assets/Scripts/Blocks/OBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBlock : Block {
+
+    //The OBlock typically does not rotate!
+    public override void Rotate()
+    {
+        return;
+    }
+
+}
